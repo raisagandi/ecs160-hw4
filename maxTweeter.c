822ffd20679771b61723651106feaa0b07c75cd5
Raisa Gandi Putri <raisagandi@gmail.com>
2019-11-26 12:05:38 -0800

    fixing some indenting issues

--- maxTweeter.c
+++ maxTweeter.c
@@ -129,55 +129,55 @@ bool fileCheck(char* fileName)
  
 void merge(int arr[], char* name_list[], int l, int m, int r)
 {
-int i, j, k;
-int n1 = m - l + 1;
-int n2 = r - m;
-int L[n1], R[n2];
-char* L1[n1];
-char* R1[n2];
-for (i = 0; i < n1; i++){
-    L[i] = arr[l + i];
-    L1[i] = name_list[l + i];   
-}
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    int L[n1], R[n2];
+    char* L1[n1];
+    char* R1[n2];
+    for (i = 0; i < n1; i++){
+        L[i] = arr[l + i];
+        L1[i] = name_list[l + i];   
+    }
 
-for (j = 0; j < n2; j++){
-    R[j] = arr[m + 1+ j];
-    R1[j] = name_list[m + 1+ j];
-}
+    for (j = 0; j < n2; j++){
+        R[j] = arr[m + 1+ j];
+        R1[j] = name_list[m + 1+ j];
+    }
 
-i = 0;
-j = 0;
-k = l;
-while (i < n1 && j < n2)
-{
-if (L[i] <= R[j])
-{
-arr[k] = L[i];
-name_list[k] = L1[i];
-i++;
-}
-else
-{
-arr[k] = R[j];
-name_list[k] = R1[j];
-j++;
-}
-k++;
-}
-while (i < n1)
-{
-arr[k] = L[i];
-name_list[k] = L1[i];
-i++;
-k++;
-}
-while (j < n2)
-{
-arr[k] = R[j];
-name_list[k] = R1[j];
-j++;
-k++;
-}
+    i = 0;
+    j = 0;
+    k = l;
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
+            arr[k] = L[i];
+            name_list[k] = L1[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = R[j];
+            name_list[k] = R1[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < n1)
+    {
+        arr[k] = L[i];
+        name_list[k] = L1[i];
+        i++;
+        k++;
+    }
+    while (j < n2)
+    {
+        arr[k] = R[j];
+        name_list[k] = R1[j];
+        j++;
+        k++;
+    }
 }
 
 void mergeSort(int arr[],char* list_name, int l, int r)
